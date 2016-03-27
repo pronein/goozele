be9800304d5cbd0d510e7d63d09e642b7279de8e
pronein <adam.dot.schrader@gmail.com>
2016-03-26 21:02:43 -0400

    Add ui-router framework

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,14 +1,6 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp')
+  .controller('View1Ctrl', [function () {
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
-}])
-
-.controller('View1Ctrl', [function() {
-
-}]);
+  }]);
